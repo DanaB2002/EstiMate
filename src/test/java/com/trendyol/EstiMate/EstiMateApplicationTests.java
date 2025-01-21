71184c8e727dd86aa506b2611493e9c7097bfa11
Dana Baeisa <danabaeisa@Danas-MacBook-Pro.local>
2025-01-21 16:16:25 +0300

    first commit empty project

---
+++ src/test/java/com/trendyol/EstiMate/EstiMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendyol.EstiMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstiMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
